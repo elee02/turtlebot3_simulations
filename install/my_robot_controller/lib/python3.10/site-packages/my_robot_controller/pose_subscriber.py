652011fca1c7d3082e73b4de964b90adbbcbddd3
elee02 <elyorrasulov1822@gmail.com>
2023-12-02 19:13:43 +0900

    Update build and setup scripts

--- install/my_robot_controller/lib/python3.10/site-packages/my_robot_controller/pose_subscriber.py
+++ install/my_robot_controller/lib/python3.10/site-packages/my_robot_controller/pose_subscriber.py
@@ -7,14 +7,14 @@ from rclpy.node import Node
 class PoseSubcriber(Node):
 
     def __init__(self):
-        super().__init__("pose_subscriber")
-        self.pose_subscriber = self.create_subscription(msg_type = Pose, topic="/turtle1/pose", self.pose_callback, 10);
+        super().__init__("pose_subscriber", parameter_overrides=[])
+        self.pose_subscriber = self.create_subscription(msg_type=Pose, topic="/turtle1/pose", callback=self.pose_callback, qos_profile=10)
 
     def pose_callback(self, msg: Pose):
         self.get_logger().info(str(msg))
 
 def main(args=None):
-    rclpy.init(args = args)
+    rclpy.init(args=args)
     node = PoseSubcriber()
     rclpy.spin(node)
     rclpy.shutdown()
